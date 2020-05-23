36fb024c0313970ffdbf9ae2774a970013067c7f
DesolateElf-dev <54561697+DesolateElf-dev@users.noreply.github.com>
2020-05-23 09:32:23 -0300

    Folder name change

---
+++ Algoritmos/modulo_01/Ex07.py
@@ -0,0 +1,14 @@
+#EXERCÍCIOS DA AULA 1 – 04/05/2020  
+#Eduardo Maciel Sanchez
+
+#7. Receba os valores do comprimento, largura e altura de um paralelepípedo. Calcule e mostre seu volume.
+
+def Ex07():
+
+    c = float(input("Digite o comprimento: "))
+    l = float(input("Digite a largura: "))
+    h = float(input("Digite a altura: "))
+
+    volume = c*l*h
+
+    print("O volume do paralelepípedo é: ",volume)
---
+++ Algoritmos/modulo_01/Ex34.py
@@ -0,0 +1,12 @@
+#EXERCÍCIOS DA AULA 3  
+#Eduardo Maciel Sanchez
+
+#34. Calcule e mostre o quadrado dos números entre 10 e 150.
+
+def Ex34():
+    
+    print("O exercício 34 calcula os quadrados dos números entre 10 e 150")
+
+    for i in range(10,151):
+        x = i*i
+        print("O quadrado de ",i," é ",x)
